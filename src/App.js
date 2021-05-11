463923fce4dea4db1919e1d7b9c25fb608291aae
ohveloper <ohveloper@gmail.com>
2021-05-11 12:59:17 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
-
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {setIsLogin, setUserInfo} from './actions/userAction'
-import { setIsLoading } from './actions/LoadingAction'
-import axios from 'axios'
-import React,{ useEffect } from "react";
-import MainPage from './pages/MainPage'
-import LoginPage from './pages/LoginPage'
-import ModalPage from './pages/ModalPage'
-import MyPage from './pages/MyPage'
-import WelcomePage from './pages/WelcomePage'
-import FollowingPage from './pages/FollowingPage'
-import Loading from './pages/Loading'
-const dotenv = require('dotenv');
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setIsLogin, setUserInfo } from "./actions/userAction";
+import { setIsLoading } from "./actions/LoadingAction";
+import axios from "axios";
+import React, { useEffect } from "react";
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import ModalPage from "./pages/ModalPage";
+import MyPage from "./pages/MyPage";
+import WelcomePage from "./pages/WelcomePage";
+import FollowingPage from "./pages/FollowingPage";
+import Loading from "./pages/Loading";
+const dotenv = require("dotenv");
 dotenv.config();
 const api = process.env.REACT_APP_SERVER_ADDRESS || "https://localhost:5000";
 
 function App() {
   const dispatch = useDispatch();
   const { isLoading } = useSelector((state) => state.recordReducer);
-  const getAccessToken = (authorizationCode)=>{
-    axios.post(api + '/main/oauth/accesstoken', { authorizationCode: authorizationCode,accept:'application/json',withCredentials:true})
-    .then(res=> {
-      return res.data.data.accessToken
-    }).then(accessToken => {
-      getUserInfo(accessToken);
-    }).catch(e => dispatch(setIsLoading(false)))
-  }
+  const getAccessToken = (authorizationCode) => {
+    dispatch(setIsLoading(false));
+    axios
+      .post(api + "/main/oauth/accesstoken", { authorizationCode: authorizationCode, accept: "application/json", withCredentials: true })
+      .then((res) => {
+        return res.data.data.accessToken;
+      })
+      .then((accessToken) => {
+        getUserInfo(accessToken);
+      })
+      .catch((e) => dispatch(setIsLoading(false)));
+  };
   const getUserInfo = (accessToken) => {
     axios
       .get("https://api.github.com/user", { headers: { authorization: `token ${accessToken}`, accept: "application/json" } })
@@ -62,9 +61,9 @@ function App() {
     const url = new URL(window.location.href);
     const authorizationCode = url.searchParams.get("code");
     if (authorizationCode) {
-      dispatch(setIsLoading(true));
       getAccessToken(authorizationCode);
     }
+    dispatch(setIsLoading(false));
   }, []);
   return isLoading ? (
     <Loading />
